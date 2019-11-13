45c1719114dba0960cf5def243d7c85b7225ac86
Catherine Harrison <mrs.wizrd@gmail.com>
2019-11-13 10:36:50 +0000

    Uploading new project

---
+++ app.js
@@ -0,0 +1,52 @@
+// 30 cards in an array.
+// split the cards up into 2 seperate arrays of 15.
+// compare stats
+// if player 1 wins, they win the 2 cards compared.
+// winner ends up with 30 cards. classes and loops.
+// doesnt need to shuffle it to hand it out yet.
+// drop to the back of the array to index zero. if index zero is greater than...
+// 30 cards slice and array distribution. Holds five stats. tally how many times you have won.
+// 
+// allow players to choose from multiple topics (difficulty - pro) :)
+// tr
+// is it player 1 turn? if not it must be player 2.
+// if try something and it doesnt work, comment it out to stop going for it again.
+//
+// 
+//
+// let playerOne = Paul;
+// let playerTwo = Jess;
+// let userInput = "click";
+
+// if (playerTwo = "turn taken") {
+//     console.log(`It is ${playerOne}'s turn`)
+// } else if (playerOne = "turn taken") {
+// console.log(`It is ${playerTwo}'s turn`)
+// } else{
+//     console.log(`Please take the next turn.`)
+// }
+//create an array of 30 cards
+//create an array of stats for each card
+let cardPack = [     
+    "Cat","Dog","Tiger","Budgie","Royal Python","Boa Constrictor","Bearded Dragon","Gecko","Fish","Leopard",     
+    "Monkey","Fox","Wolf","Cobra","Eagle","Dolphin","Tarantula","Rabbit","Guinea Pig","Rat",     
+    "Hamster","Harvest Mouse","Milipede","Fruit Fly","Bat","Polar Bear","Penguin","Chinchilla","Ferret","Maine Coon"
+//30 cards for my pack are here
+];
+// insert.pop();
+console.log(cardPack);
+
+var animalStats = 
+
+switch(myName)
+{
+    case'Jacob':
+    console.log(`Hi, ${myName}`);
+    break;
+    case 'Stuart':
+    console.log(`Hi, ${myName}`);
+    break;
+    default:
+    console.log("Who are you?");
+    break;
+}
